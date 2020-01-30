4d04715478e6d33ce4d833dd7fd08a909c7f1684
BookeeM <bouckiemilien@gmail.com>
2020-01-31 00:10:17 +0100

    Correction de bugs, ajout de contenu

---
+++ core/src/fr/bouckdev/game/Sprite/mobs/Mobs.java
@@ -0,0 +1,45 @@
+package fr.bouckdev.game.Sprite.mobs;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.World;
+
+import fr.bouckdev.game.Screens.PlayScreen;
+
+public abstract class Mobs extends Sprite {
+
+	protected World world;
+	protected PlayScreen screen;
+	public Body b2body;
+	public Vector2 velocity;
+	
+	
+	public Mobs(PlayScreen screen, float x, float y,boolean vole) {
+		
+		this.world = screen.getWorld();
+		this.screen = screen;
+		setPosition(x,y);
+		defineMob();
+		b2body.setActive(false);
+		velocity = new Vector2(-1, -2);	
+	}
+	
+	protected abstract void defineMob();
+	public abstract void update(float dt);
+	public abstract void hitOnHead();
+	
+	
+	public void reverseVelocity(boolean x, boolean y) {
+		
+		if(x)
+			velocity.x = -velocity.x;
+		if(y)
+	
+			velocity.y = -velocity.y;
+	}
+	
+	
+	
+	
+}
